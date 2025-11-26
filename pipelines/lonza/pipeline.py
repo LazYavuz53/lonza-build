826a277df7efb35f69df8f323c8544ad45ba876c
Yavuz <yavuzkulaber53@hotmail.com>
2025-11-26 14:02:25 +0000

    adjusted pipeline py

--- pipelines/lonza/pipeline.py
+++ pipelines/lonza/pipeline.py
@@ -174,7 +174,7 @@ def get_pipeline(
 
     sklearn_processor = SKLearnProcessor(
         framework_version="0.23-1",
-        instance_type=processing_instance_type_param,
+        instance_type=processing_instance_type_param.default_value,
         instance_count=processing_instance_count,
         base_job_name=f"{base_job_prefix}/sklearn-LonzaClinicalMarker-preprocess",
         sagemaker_session=pipeline_session,
